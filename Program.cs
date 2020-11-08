50fba8c8c9390f8e21020c9b30ded7b1a38b922f
Gessielle <gessielle-barossi@hotmail.com>
2020-11-07 23:17:59 -0300

    maior 2 números

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Maior_2_Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero;
+            double maior = 0;
+
+            Console.WriteLine("Digite o 1º Número: ");
+            numero = double.Parse(Console.ReadLine());
+            if (numero > maior)
+            {
+                maior = numero;
+            }
+
+            Console.Write("Digite o 2º Número: ");
+            numero = double.Parse(Console.ReadLine ());
+            if (numero > maior)
+            {
+                Console.WriteLine();
+                maior = numero;
+            }
+            Console.WriteLine("O maior número digitado é: {0} " , maior);
+
+            Console.ForegroundColor=ConsoleColor.Green;
+            Console.WriteLine ();
+            Console.WriteLine ( " Pressione ENTER para sair! ");
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+    }
+}
